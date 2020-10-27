817fdf69875adb7c1b6cc14f72ba4c4e90b66e04
OneSunLights <69711785+OneSunLights@users.noreply.github.com>
2020-10-27 14:27:10 +0400

    Add files via upload

---
+++ local/templates/style/components/bitrix/menu/main_menu/template.php
@@ -0,0 +1,37 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<?if (!empty($arResult)):?>
+
+
+<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+	<nav class="link-effect-7" id="link-effect-7">
+		<ul class="nav navbar-nav">
+			<? foreach($arResult as $arItem):
+					if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
+						continue;
+			 ?>
+			 <? if($arItem["SELECTED"]): ?>
+				<li class="active act"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+			<? else: ?>
+				<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+			<? endif; ?>
+			<? endforeach; ?>
+		</ul>
+	</nav>
+</div>
+<!-- <ul class="left-menu">
+<?
+foreach($arResult as $arItem):
+	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
+		continue;
+?>
+	<?if($arItem["SELECTED"]):?>
+		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
+	<?else:?>
+		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+	<?endif?>
+	
+<?endforeach?>
+
+</ul> -->
+<?endif?>
