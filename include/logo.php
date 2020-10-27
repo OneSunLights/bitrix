2480e710fe7e2039e5158d60cf1c46b0b43fdbf7
OneSunLights <69711785+OneSunLights@users.noreply.github.com>
2020-10-27 14:32:43 +0400

    Add files via upload

--- include/logo.php
+++ include/logo.php
@@ -1,1 +1,7 @@
-
+<div class="logo wow fadeInDown" data-wow-duration=".8s" data-wow-delay=".2s">
+	<h1><a href="/">STYLE BLOG</a></h1>
+	<p>
+ <label class="of"></label>LET'S MAKE A PERFECT STYLE<label class="on"></label>
+	</p>
+</div>
+ <br>
