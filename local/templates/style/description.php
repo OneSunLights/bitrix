cfdb902f5b437082490b3264c8f3fb9318254167
OneSunLights <69711785+OneSunLights@users.noreply.github.com>
2020-10-27 14:16:13 +0400

    Add files via upload

--- local/templates/style/description.php
+++ local/templates/style/description.php
@@ -1,1 +1,7 @@
-
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?php
+$arTemplate = [
+	"NAME" => "Блог стиля",
+	"DESCRIPTION" => "Шаблон блога стиля"
+]
+?>
---
+++ local/templates/style/header.php
@@ -0,0 +1,107 @@
+<?
+	use \Bitrix\Main\Page\Asset;
+	if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title><?php $APPLICATION->ShowTitle(); ?></title>
+<?php
+	$APPLICATION->ShowHead();
+	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/bootstrap.css");
+	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
+	CJSCore::Init(['jquery']);
+	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/bootstrap.min.js");
+	Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1">');
+	Asset::getInstance()->addString("<link href='//fonts.googleapis.com/css?family=Raleway:400,600,700' rel='stylesheet' type='text/css'>");
+	// Asset::getInstance()->addString('<link type="image/x-icon" href="'.SITE_TEMPLATE_PATH.'/favicon.ico" rel="shortcut icon" //>');
+	//$APPLICATION->SetAdditionalCSS();
+?>
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<!-- animation-effect -->
+<link href="css/animate.min.css" rel="stylesheet"> 
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!-- //animation-effect -->
+</head>
+<body>
+	<div id="panel"><?php $APPLICATION->ShowPanel(); ?></div>
+<div class="header" id="ban">
+		<div class="container">
+			<div class="head-left wow fadeInLeft animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+				<div class="header-search">
+						<div class="search">
+							<input class="search_box" type="checkbox" id="search_box">
+							<label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
+							<div class="search_form">
+								<form action="#" method="post">
+									<input type="text" name="Search" placeholder="Search...">
+									<input type="submit" value="Send">
+								</form>
+							</div>
+						</div>
+				</div>
+			</div>
+			<div class="header_right wow fadeInLeft animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<?$APPLICATION->IncludeComponent("bitrix:menu", "main_menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "1",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
+		),
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "N",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "main",	// Тип меню для первого уровня
+		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+	),
+	false
+);?>
+			</nav>
+			</div>
+			<div class="nav navbar-nav navbar-right social-icons wow fadeInRight animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInRight;">
+					<ul>
+						<li><a href="#"> </a></li>
+						<li><a href="#" class="pin"> </a></li>
+						<li><a href="#" class="in"> </a></li>
+						<li><a href="#" class="be"> </a></li>
+						
+						<li><a href="#" class="vimeo"> </a></li>
+					</ul>
+				</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<!--start-main-->
+	<div class="header-bottom">
+		<div class="container">
+			<?$APPLICATION->IncludeComponent(
+				"bitrix:main.include",
+				"",
+				Array(
+					"AREA_FILE_SHOW" => "file",
+					"AREA_FILE_SUFFIX" => "inc",
+					"EDIT_TEMPLATE" => "",
+					"PATH" => "/include/logo.php"
+				)
+			);?>
+		</div>
+	</div>
