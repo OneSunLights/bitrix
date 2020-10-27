dfdcc1c33a98ae345c68aa460367e1db5a3aede8
OneSunLights <69711785+OneSunLights@users.noreply.github.com>
2020-10-27 14:35:55 +0400

    Add files via upload

--- local/templates/style/components/bitrix/menu/main_menu/template.php
+++ local/templates/style/components/bitrix/menu/main_menu/template.php
@@ -19,19 +19,4 @@
 		</ul>
 	</nav>
 </div>
-<!-- <ul class="left-menu">
-<?
-foreach($arResult as $arItem):
-	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
-		continue;
-?>
-	<?if($arItem["SELECTED"]):?>
-		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
-	<?else:?>
-		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-	<?endif?>
-	
-<?endforeach?>
-
-</ul> -->
 <?endif?>
